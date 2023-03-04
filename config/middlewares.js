c8fe7749c11d7ac62d231c3391b9663f07dfcf0d
neo-noire <111647919+neo-noire@users.noreply.github.com>
2023-03-04 15:53:48 +0200

    clien fvdt

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,14 +2,6 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::poweredBy',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:1337', 'https://lobster-app-8giew.ondigitalocean.app', 'http://matusshop.online', 'http://localhost:3000']
-    }
-  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
