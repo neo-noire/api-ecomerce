ee0bc0eb2e707e39c2f04350e0ba8b4c63f171f2
neo-noire <111647919+neo-noire@users.noreply.github.com>
2023-03-05 14:14:49 +0200

    m

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,14 @@
+// module.exports = ({ env }) => ({
+//     proxy: true,
+//     url: env('APP_URL'), // Sets the public URL of the application.
+//     app: { 
+//       keys: env.array('APP_KEYS')
+//     },
+// });
+
 module.exports = ({ env }) => ({
-    proxy: true,
-    url: env('APP_URL'), // Sets the public URL of the application.
-    app: { 
-      keys: env.array('APP_KEYS')
-    },
+  url: env("RENDER_EXTERNAL_URL"),
+  dirs: {
+    public: "/data/public"
+  },
 });
