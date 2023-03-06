b70a388d9cfc04f440f10fc961abeb2ef3c97f3f
neo-noire <111647919+neo-noire@users.noreply.github.com>
2023-03-06 13:59:26 +0200

    db settings

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      host: env('DATABASE_HOST', 'app-31861ba3-6356-4d8e-97ac-6f0afe958025-do-user-13686326-0.b.db.ondigitalocean.com'),
+      port: env.int('DATABASE_PORT', 25060),
+      database: env('DATABASE_NAME', 'db'),
+      user: env('DATABASE_USERNAME', 'db'),
+      password: env('DATABASE_PASSWORD', 'AVNS_b0VOe8sEv6nXOsXQzjv'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
